34e3aba6e2d0cf70f4eefa0a4a318b0bae7808e9
cipuzp <zhupan97njy@gmail.com>
2022-12-08 21:03:40 +0800

    fix bugs

--- pages/supplierInbound/supplierInbound.js
+++ pages/supplierInbound/supplierInbound.js
@@ -1,17 +1,92 @@
 // pages/supplierInbound/supplierInbound.js
+
+// const order = ['scroll1', 'scroll2', 'scroll3', 'scroll4']
+
 Page({
 
     data: {
         activeValues: [0],
         backTopTheme: 'round',
         backTopText: '顶部',
+        originFiles: [
+
+        ],
+        originFiles2: [
+
+        ],
+        gridConfig: {
+            column: 4,
+            width: 160,
+            height: 160,
+        },
+        gridConfig2: {
+            column: 4,
+            width: 160,
+            height: 160,
+        }
     },
     onLoad(options) {
 
     },
+    formSubmit(e) {
+        console.log(e)
+    },
+    onChange1(e) {
+        console.log('radio', e.detail);
+    },
+    onChange2(e) {
+        console.log('radio', e.detail);
+    },
     handleChange(e) {
+        console.log('handleChange', e);
+    },
+
+    handleSuccess(e) {
+        const {
+            files
+        } = e.detail;
         this.setData({
-            activeValues: e.detail.value,
+            originFiles: files,
         });
+    },
+    handleRemove(e) {
+        const {
+            index
+        } = e.detail;
+        const {
+            originFiles
+        } = this.data;
+        originFiles.splice(index, 1);
+        this.setData({
+            originFiles,
+        });
+    },
+    handleClick(e) {
+        console.log(e.detail.file);
+    },
+
+    handleSuccess2(e) {
+        const {
+            files
+        } = e.detail;
+        this.setData({
+            originFiles2: files,
+        });
+    },
+    handleRemove2(e) {
+        const {
+            index
+        } = e.detail;
+        const {
+            originFiles2
+        } = this.data;
+        originFiles2.splice(index, 1);
+        this.setData({
+            originFiles2,
+        });
+    },
+    handleClick2(e) {
+        console.log(e.detail.file);
     }
+
 })
